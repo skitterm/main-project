9a8e05739e60b0e5f261c5e59b305635e115c02c
skitterm <skitterman@esri.com>
2018-01-01 15:55:30 -0800

    Area code accounted for.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const number = document.getElementById('number');
 
-number.innerHTML = '(123) 456-7890';
+const sameAreaCode = false;
+let areaCode = '';
+
+if (!sameAreaCode) {
+  areaCode = '(123) ';
+}
+
+number.innerHTML = `${areaCode}456-7890`;
